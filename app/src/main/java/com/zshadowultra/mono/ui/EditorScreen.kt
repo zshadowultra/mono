4fed8d10ccc90b88b27ce352783327d680f14d7c
zshadowultra <zshadowultra@users.noreply.github.com>
2026-08-26 14:42:21 +0530

    fix: missing border import

--- app/src/main/java/com/zshadowultra/mono/ui/EditorScreen.kt
+++ app/src/main/java/com/zshadowultra/mono/ui/EditorScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
